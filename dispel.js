ee1a7f775c76587aeeb5730b53973243c951658e
DESKTOP-47LOHSE\Daniel <est.daniel.pedroza@unimilitar.com>
2024-11-20 16:21:07 -0500

    Home

--- dispel.js
+++ dispel.js
@@ -1,3 +1,4 @@
+let body=document.querySelector('.body');
 let container= document.querySelector('.welcome');
 let video= document.querySelector('.welcome__video');
 
@@ -5,4 +6,10 @@ let video= document.querySelector('.welcome__video');
 video.addEventListener('ended', () => {
     console.log('El video ha terminado de reproducirse.');
     video.classList.toggle('video__end');
+    document.body.style.overflow = 'auto';
 });
+
+window.addEventListener('pagehide', function () {
+    window.scrollTo(0, 0);
+});
+
