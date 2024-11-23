e76a516601581f2ec652e67a818449f7d05b9f10
DESKTOP-47LOHSE\Daniel <est.daniel.pedroza@unimilitar.com>
2024-11-23 03:01:04 -0500

    Revome Video in mobile

--- Scripts/sign_up.js
+++ Scripts/sign_up.js
@@ -3,37 +3,65 @@ const btnno = document.querySelector('.btn-no');
 
 const audio = document.querySelector('.welcome__video');
 
-const muteadito=document.querySelector('.mutear')
-
-
 var silencio = (audio.muted = false);
+
+let veil = document.querySelector('.veil');
+
+
+let screenWidth = screen.width;
+const minWidth= 225;
+const maxWidth= 762;
+
+
+const dispel=document.getElementById('dispel');
 
 
 btnsi.addEventListener('click', function reproducir() {
 
-    audio.muted = false;
-    audio.play();
-    veil.style.zIndex="-1";
-    
-    
+    if (screenWidth >= minWidth && screenWidth<=maxWidth) {
 
+        audio.remove();
+        dispel.remove();
+        veil.style.zIndex="-1";
+        console.log('Video eliminado correctamente.');
+        document.body.style.overflow = 'auto';
+        
+    }else{
+        audio.muted = false;
+        audio.play();
+        document.body.style.overflow = 'auto';
 
+    }
+    
 })
 
 btnno.addEventListener('click', function reproducir() {
 
 
-    audio.muted = true;
-    audio.play();
-    veil.style.zIndex="-1";
-    silencio = true;
-    window.open('nonono.html');
+    if (screenWidth >= minWidth && screenWidth<=maxWidth) {
+
+        audio.remove();
+        veil.style.zIndex="-1";
+        console.log('Video eliminado correctamente.');
+        document.body.style.overflow = 'auto';
+        window.open('nonono.html');
+        
+    }else{
+        audio.muted = true;
+        audio.play();
+        veil.style.zIndex="-1";
+        silencio = true;
+        window.open('nonono.html');
+        document.body.style.overflow = 'auto';
+
+    }
+    
     // window.close();
 
 })
 
 
-let veil = document.querySelector('.veil');
+
 
 
 muteadito.addEventListener('click', function silenciar(){
