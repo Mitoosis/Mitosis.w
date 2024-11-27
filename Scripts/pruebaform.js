04f7df5bdf61a5e3fbb81ec624e0aa4b0dd0f2de
DESKTOP-47LOHSE\Daniel <est.daniel.pedroza@unimilitar.com>
2024-11-26 23:21:46 -0500

    New__Form

--- Scripts/pruebaform.js
+++
@@ -1,8 +0,0 @@
-const btnno = document.querySelector('.form__button');
-btnno.addEventListener('click', function reproducir() {
-
-    window.close();
-    window.open('../Pages/close.html');
-    
-
-})
